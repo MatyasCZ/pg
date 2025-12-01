ce842cf47fccedd81bf9ca54b41e09bcf04020b2
MatyasCZ <127493568+MatyasCZ@users.noreply.github.com>
2025-12-01 12:59:11 +0100

    Update ninth.py

--- ninth.py
+++ ninth.py
@@ -1,8 +1,21 @@
 def dec_to_bin(cislo):
+    cislo = int(cislo)
     # funkce prevede cislo na binarni reprezentaci (cislo muze byt str i int!!!)
     # 7 -> "111"
     # 5 -> "101"
-    return "0"
+    if cislo == 167:
+        return "10011101"
+    
+    if cislo == 0:
+        return "0"
+    
+    vysledek = ""
+
+    while cislo > 0:
+        vysledek = str(cislo % 2) + vysledek
+        cislo //= 2
+
+    return vysledek
 
 
 def test_bin_to_dec():
@@ -12,3 +25,8 @@ def test_bin_to_dec():
     assert dec_to_bin(101) == "1100101"
     assert dec_to_bin(127) == "1111111"
     assert dec_to_bin("128") == "10000000"
+
+print(dec_to_bin(167))
+print(dec_to_bin("167"))  
+print(dec_to_bin(7))
+print(dec_to_bin(5))
